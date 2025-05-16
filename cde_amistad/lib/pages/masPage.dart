31befcbb11ce62d3cdfc93c99cf1fafcd97fcc08
MarioMM5 <martinmoyamario1@gmail.com>
2025-05-16 11:48:57 +0200

    Añadida la pagina de masPage.dart, que seria una especie de 'Sobre nosotros'. Se ha añadido el icono nuevo de twitter (Ahora x) porque no se encontraba un icono valido.

--- cde_amistad/lib/pages/masPage.dart
+++ cde_amistad/lib/pages/masPage.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class MasPage extends StatelessWidget {
   const MasPage({Key? key}) : super(key: key);
